d846b88416d70903a357d5caa02d1d14c8595e33
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-03-11 08:51:54 +0530

    Gambling uc1

---
+++ Gambling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Gambling
+{
+    public class Gambling
+
+    {
+        public void Display()
+        {
+            int Bet = 1;
+            int Money = 100;
+            Random bet_num = new Random();
+            int check = bet_num.Next(0, 100);
+            Console.WriteLine("Started ");
+            Console.WriteLine("check");
+           
+        }
+
+        public static void Main(string[] args)
+{
+            Gambling gam = new Gambling();
+            gam.Display();
+}
+
+         }
+        
+    }
+
