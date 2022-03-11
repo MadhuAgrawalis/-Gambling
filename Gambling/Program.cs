bbe6a7f0f9ed574cd586162f508d706b3dcf300c
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-03-11 11:47:32 +0530

    50% stack

--- Gambling/Program.cs
+++ Gambling/Program.cs
@@ -5,30 +5,53 @@ namespace Gambling
     public class Gambling
 
     {
-        public void Display()
+        /*  public void Display()
+          {
+              int Bet = 1;
+              int Money = 100;
+              Random bet_num = new Random();
+              int check = bet_num.Next(0, 2);
+              Console.WriteLine("Started ");
+              Console.WriteLine("check");
+
+
+              if (Bet == check)
+              {
+                  Console.WriteLine("you Win");
+              }
+              else
+              {
+                  Console.WriteLine("Loss");
+              }
+          }*/
+        public void play()
         {
             int Bet = 1;
             int Money = 100;
-            Random bet_num = new Random();
-            int check = bet_num.Next(0, 100);
-            Console.WriteLine("Started ");
-            Console.WriteLine("check");
-
-        
-        if (Bet == check)
+            int lose = 0;
+            int win = 1;
+            int left = 100;
+            Random random = new Random();
+            int option = random.Next(0, 2);
+            while (left > 50 && left < 150)
             {
-            Console.WriteLine("you Win");
+                switch (option)
+                {
+                    case lose:
+                        left = left - Bet;
+                        break;
+                    case win:
+                        left = left + Bet;
+                        break;
+                }
             }
-          else 
-           {
-            Console.WriteLine("Loss");
-            }
+            Console.WriteLine("left is " + left);
         }
 
-        public static void Main(string[] args)
+                public static void Main(string[] args)
 {
             Gambling gam = new Gambling();
-            gam.Display();
+            gam.play();
 }
 
          }
